df1b9ec3a6295d5d1eb7aac7a6198b489be96374
ahmetemincopur <64459050+ahmetemincopur@users.noreply.github.com>
2024-06-05 18:54:43 +0300

    Resoures added for to not show any unwanted data. Pagination component added. Projects listed on Project page.

--- ProjectManagementApp/resources/js/Pages/Project/Index.jsx
+++ ProjectManagementApp/resources/js/Pages/Project/Index.jsx
@@ -1,5 +1,7 @@
+import Pagination from "@/Components/Pagination";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
-import { Head } from "@inertiajs/react";
+import { PROJECT_STATUS_CLASS_MAP, PROJECT_STATUS_TEXT_MAP } from "@/Constants";
+import { Head, Link } from "@inertiajs/react";
 
 export default function Index({ auth, projects }) {
     return (
@@ -12,7 +14,49 @@ export default function Index({ auth, projects }) {
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">Projectsssss</div>
+                        <div className="p-6 text-gray-900">
+
+                            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:gray-700 dark:text-gray-400 border-b-2 border-b-gray-500">
+                                    <tr className="text-nowrap">
+                                        <th className="px-3 py-2">ID</th>
+                                        <th className="px-3 py-2">Image</th>
+                                        <th className="px-3 py-2">Name</th>
+                                        <th className="px-3 py-2">Status</th>
+                                        <th className="px-3 py-2">Create Date</th>
+                                        <th className="px-3 py-2">Due Date</th>
+                                        <th className="px-3 py-2">Created By</th>
+                                        <th className="px-3 py-2 text-right">Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {projects.data.map(project => (
+                                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700" key={project.id}>
+                                            <td className="px-3 py-3">{project.id}</td>
+                                            <td className="px-3 py-3"><img src={project.image_path} alt="" style={{ width: 60 }} /></td>
+                                            <td className="px-3 py-3">{project.name}</td>
+                                            <td className="px-3 py-3">
+                                                <span className={"px-2 py-1 rounded text-white " + PROJECT_STATUS_CLASS_MAP[project.status]}>
+                                                    {PROJECT_STATUS_TEXT_MAP[project.status]}
+                                                </span>
+                                            </td>
+                                            <td className="px-3 py-3 text-nowrap">{project.created_at}</td>
+                                            <td className="px-3 py-3 text-nowrap">{project.due_date}</td>
+                                            <td className="px-3 py-3">{project.createdBy.name}</td>
+                                            <td className="px-3 py-3">
+                                                <Link href={route("project.edit", project.id)} className="font-medium text-blue-600 dark:text-blue-500 hover:underline mx-1">
+                                                    Edit
+                                                </Link>
+                                                <Link href={route("project.destroy", project.id)} className="font-medium text-red-600 dark:text-red-500 hover:underline mx-1">
+                                                    Delete
+                                                </Link>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                            <Pagination links={projects.meta.links} />
+                        </div>
                     </div>
                 </div>
             </div>

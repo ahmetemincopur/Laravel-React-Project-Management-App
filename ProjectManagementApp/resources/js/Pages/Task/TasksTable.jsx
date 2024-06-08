150e30d8aa03eba54c0ac5d64a2aac9d05876673
ahmetemincopur <64459050+ahmetemincopur@users.noreply.github.com>
2024-06-08 11:57:15 +0300

    Task delete issue fixed.

--- ProjectManagementApp/resources/js/Pages/Task/TasksTable.jsx
+++ ProjectManagementApp/resources/js/Pages/Task/TasksTable.jsx
@@ -7,6 +7,7 @@ import { Link, router } from "@inertiajs/react";
 
 export default function TasksTable({
     tasks,
+    success,
     queryParams = null,
     hideProjectColumn = false,
 }) {
@@ -41,8 +42,20 @@ export default function TasksTable({
         router.get(route("task.index"), queryParams);
     };
 
+    const deleteTask = (task) => {
+        if (!window.confirm("Are you sure you want to delete the task?")) {
+            return;
+        }
+        router.delete(route("task.destroy", task.id));
+    };
+
     return (
         <>
+            {success && (
+                <div className="bg-emerald-500 py-2 px-4 text-white rounded mb-4">
+                    {success}
+                </div>
+            )}
             <div className="overflow-auto">
                 <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                     <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500">
